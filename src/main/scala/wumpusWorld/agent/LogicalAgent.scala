205ed87d905954b0fcbdb077a1e370ab126445ba
Larry Simon <larry.simon@inflectiongroup.com>
2020-09-27 15:01:56 -0400

    Removed dependency on scalapy.

--- src/main/scala/wumpusWorld/agent/LogicalAgent.scala
+++ src/main/scala/wumpusWorld/agent/LogicalAgent.scala
@@ -1,7 +1,6 @@
 package wumpusWorld.agent
 
 import wumpusWorld.environment.{Action, Forward, Percept}
-import me.shadaj.scalapy.py
 
 class LogicalAgent extends Agent {
 
@@ -9,7 +8,6 @@ class LogicalAgent extends Agent {
   val dumbDumb = new NaiveAgent();
 
   def nextAction(percept: Percept): Action = {
-    println(py.global.len(List(1, 2, 3)))
     Forward
   }
 
